d1ca6c5473c56f92ec87aba48034cf2c0d68e4f9
Igor Onyshchenko <optimum.flaminis@gmail.com>
2019-02-23 23:40:08 +0200

    feat: remove unnecessary files and add react router

---
+++ src/ui/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+
+const signedInRoutes: React.Component[] = [
+  // <Route path="/" exact component={Products} />
+];
+
+const signedOutRoutes: React.Component[] = [
+  // <Route path="/login" exact component={Login} />,
+  // <Route path="/register" exact component={Register} />,
+  // <Route path="/" render={() => <Redirect to={{ pathname: '/login' }} />} />,
+];
+
+const App: React.FC<{ authorized: boolean }> = ({ authorized }) => (
+  <div className="app">
+    <Header />
+    <BrowserRouter>
+      <Switch>{authorized ? signedInRoutes : signedOutRoutes}</Switch>
+    </BrowserRouter>
+  </div>
+);
+
+App.defaultProps = {
+  authorized: false,
+};
+
+export default App;
