39602afded4070d15d471a908926ff441d477628
Viacheslav Zhuravskyi <viacheslav.zhuravskyi@gmail.com>
2019-02-23 18:15:31 +0200

    feat: implement AuthService

---
+++ src/services/api-provider/auth/AuthService.ts
@@ -0,0 +1,49 @@
+import AbstractAuthService from "./AbstractAuthService";
+import User from "../../models/User";
+import endpoint from "../../endpoint";
+
+class AuthService implements AbstractAuthService {
+  public signIn(login: string, password: string): Promise<User> {
+    return fetch(endpoint + "/oauth/token", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        login,
+        password
+      })
+    }).then(res => res.json());
+  }
+
+  public signUp(name: string, login: string, password: string): Promise<User> {
+    return fetch(endpoint + "/oauth/register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        login,
+        password
+      })
+    }).then(res => res.json());
+  }
+
+  public signOut(token: string): Promise<void> {
+    return fetch(endpoint + "/oauth/token", {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token
+      })
+    }).then(res => res.json());
+  }
+}
+
+export default AuthService;
