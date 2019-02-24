b48178259dc6174a82a33684727ed067d3c06c96
Viacheslav Zhuravskyi <viacheslav.zhuravskyi@gmail.com>
2019-02-24 05:28:02 +0200

    feat: implement products actions

--- src/store/actions/products.ts
+++ src/store/actions/products.ts
@@ -22,3 +22,30 @@ export interface ExecuteProductAction {
   type: ProductsActionType.EXECUTE_PRODUCT;
   payload: { executeRequest: RequestViewModel };
 }
+
+export type ProductsAction =
+  | GetProductsAction
+  | CreateProductAction
+  | ExecuteProductAction;
+
+export const getProducts = (request: string): GetProductsAction => ({
+  type: ProductsActionType.GET_PRODUCTS,
+  payload: { request },
+});
+
+export const createProduct = (
+  name: string,
+  description: string,
+  inputs: InputViewModel,
+  outputs: OutputViewModel
+): CreateProductAction => ({
+  type: ProductsActionType.CREATE_PRODUCT,
+  payload: { name, description, inputs, outputs },
+});
+
+export const executeProduct = (
+  executeRequest: RequestViewModel
+): ExecuteProductAction => ({
+  type: ProductsActionType.EXECUTE_PRODUCT,
+  payload: { executeRequest },
+});
