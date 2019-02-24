9122e1d8a41ae7b662fe142bbd28a64c068208a1
Igor Onyshchenko <optimum.flaminis@gmail.com>
2019-02-24 02:28:55 +0200

    feat: add layout to Profile

--- src/ui/pages/Profile.tsx
+++ src/ui/pages/Profile.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Profile = () => <div />;
+const Profile = () => (
+  <div className="profile">
+    <div className="profile__left" />
+    <div className="profile__right" />
+  </div>
+);
 
 export default Profile;
