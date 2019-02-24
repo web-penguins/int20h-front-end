cf3fa7623325468c4120bfebbb6cdae707e9797d
Viacheslav Zhuravskyi <viacheslav.zhuravskyi@gmail.com>
2019-02-24 05:57:06 +0200

    feat: add products reducer to root reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
+import { products, ProductsState } from './products';
 import { user, UserState } from './user';
 
 export interface AppState {
   user: UserState;
+  products: ProductsState;
 }
 
 export default combineReducers<AppState>({
   user,
+  products,
 });
