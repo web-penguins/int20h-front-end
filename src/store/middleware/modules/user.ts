c9e8d97ff8e0d377975b991b00ea36151def0582
Igor Onyshchenko <optimum.flaminis@gmail.com>
2019-02-24 09:57:32 +0200

    fix: mock user service comeback

--- src/store/middleware/modules/user.ts
+++ src/store/middleware/modules/user.ts
@@ -1,9 +1,11 @@
 import { MiddlewareAPI } from 'redux';
 import MockUserService from '../../../services/user/MockUserService';
-import user from '../../../services/user/userSingleton';
+// import user from '../../../services/user/userSingleton';
 import { getProducts } from '../../actions/products';
 import { setUser, SignInAction } from '../../actions/user';
 import { UserActionType } from '../../constants/user';
+
+const user = new MockUserService();
 
 const auth = (action: SignInAction, store: MiddlewareAPI) => {
   const { login, password } = action.payload;
