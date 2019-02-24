d6ebd5cc9531d12124ae29b3d0e15de5ad29cd78
Viacheslav Zhuravskyi <viacheslav.zhuravskyi@gmail.com>
2019-02-24 07:27:35 +0200

    feat: create base of Product page

---
+++ src/ui/pages/Product.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ProductViewModel from '../../services/models/Product';
+import { AppState } from '../../store/reducers';
+import Button from '../components/Button';
+
+interface Props {
+  product: ProductViewModel;
+}
+
+const Product: React.FC<Props> = ({ product }) => {
+  return (
+    <div className="product">
+      <div className="product-inputs">
+        {product.inputs.map(inp => {
+          return (
+            <div className="product-input">
+              <label htmlFor={inp.name} />
+              <input
+                key={inp.name}
+                id={inp.name}
+                type={inp.inputType === 'string' ? 'string' : 'file'}
+              />
+              <p>{inp.description}</p>
+            </div>
+          );
+        })}
+      </div>
+      <Button text="Execute" onClick={() => {}} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state: AppState, ownProps: any): Props => ({
+  product: state.products.products!.filter(
+    pr => pr.productId === ownProps.match.params.productId
+  )[0],
+});
+
+export default connect(mapStateToProps)(Product);
