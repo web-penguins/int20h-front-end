55ce6d3b05eaceb67fb31ffd5c29265224323807
Viacheslav Zhuravskyi <viacheslav.zhuravskyi@gmail.com>
2019-02-24 05:46:19 +0200

    feat: add saving actions and types

--- src/store/actions/products.ts
+++ src/store/actions/products.ts
@@ -1,11 +1,18 @@
 import { InputViewModel } from '../../services/models/Input';
 import { OutputViewModel } from '../../services/models/Output';
+import ProductViewModel from '../../services/models/Product';
 import RequestViewModel from '../../services/models/Request';
+import { ResultViewModel } from '../../services/models/Result';
 import { ProductsActionType } from '../constants/products';
 
 export interface GetProductsAction {
   type: ProductsActionType.GET_PRODUCTS;
   payload: { request: string };
+}
+
+export interface SaveGotProductsAction {
+  type: ProductsActionType.SAVE_GOT_PRODUCTS;
+  payload: { products: ProductViewModel[] };
 }
 
 export interface CreateProductAction {
@@ -18,19 +25,41 @@ export interface CreateProductAction {
   };
 }
 
+export interface SaveCreatedProductAction {
+  type: ProductsActionType.SAVE_CREATED_PRODUCT;
+  payload: {
+    product: ProductViewModel;
+  };
+}
+
 export interface ExecuteProductAction {
   type: ProductsActionType.EXECUTE_PRODUCT;
   payload: { executeRequest: RequestViewModel };
 }
 
+export interface SaveExecutedProductResultAction {
+  type: ProductsActionType.SAVE_EXECUTED_PRODUCT_RESULT;
+  payload: { executionResult: ResultViewModel };
+}
+
 export type ProductsAction =
   | GetProductsAction
+  | SaveGotProductsAction
   | CreateProductAction
-  | ExecuteProductAction;
+  | SaveCreatedProductAction
+  | ExecuteProductAction
+  | SaveExecutedProductResultAction;
 
 export const getProducts = (request: string): GetProductsAction => ({
   type: ProductsActionType.GET_PRODUCTS,
   payload: { request },
+});
+
+export const saveGotProducts = (
+  products: ProductViewModel[]
+): SaveGotProductsAction => ({
+  type: ProductsActionType.SAVE_GOT_PRODUCTS,
+  payload: { products },
 });
 
 export const createProduct = (
@@ -43,9 +72,23 @@ export const createProduct = (
   payload: { name, description, inputs, outputs },
 });
 
+export const saveCreatedProduct = (
+  product: ProductViewModel
+): SaveCreatedProductAction => ({
+  type: ProductsActionType.SAVE_CREATED_PRODUCT,
+  payload: { product },
+});
+
 export const executeProduct = (
   executeRequest: RequestViewModel
 ): ExecuteProductAction => ({
   type: ProductsActionType.EXECUTE_PRODUCT,
   payload: { executeRequest },
+});
+
+export const saveExecutedProductResult = (
+  executionResult: ResultViewModel
+): SaveExecutedProductResultAction => ({
+  type: ProductsActionType.SAVE_EXECUTED_PRODUCT_RESULT,
+  payload: { executionResult },
 });
