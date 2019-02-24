1029ff2e9a61c57096d3fdc5ebdbaa9234bda511
Igor Onyshchenko <optimum.flaminis@gmail.com>
2019-02-24 04:18:09 +0200

    feat: implement profile page and ProductItem

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const Button: React.FC<{ text: string; onClick: () => void }> = ({
-  text,
-  onClick,
-}) => (
-  <button type="button" className="button login__button" onClick={onClick}>
+const Button: React.FC<{
+  text: string;
+  onClick: () => void;
+  variant?: string;
+  className?: string;
+}> = ({ text, onClick, variant, className }) => (
+  <button
+    type="button"
+    className={`button button_${variant} ${className}`}
+    onClick={onClick}
+  >
     {text}
   </button>
 );
+
+Button.defaultProps = {
+  variant: 'default',
+};
 
 export default Button;
